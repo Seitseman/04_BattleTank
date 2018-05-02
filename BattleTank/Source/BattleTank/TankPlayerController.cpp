25bf134d89ba31bc5794162470dd78d3c298672a
Seitseman <Seitseman@users.noreply.github.com>
2018-05-02 04:21:03 +0300

    Added some checking to the TankPlayerController to see if the tank is possesed at the BeginPlay

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -7,3 +7,20 @@ ATank * ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ContrrolledTank = GetControlledTank();
+    if (!ContrrolledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController NOT possesing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController possesing a tank %s"), *ContrrolledTank->GetName());
+    }
+
+    UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController::BeginPlay"));
+}
